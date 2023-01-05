09d7188bfdb860fd3e14489721087cd5f47ead12
lgorvin <lgorvin78@gmail.com>
2023-01-05 13:37:59 +0000

    Template

---
+++ Vite-TS-Tailwind/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-center text-white text-5xl font-bold">
+        React - TypeScript - Tailwind CSS
+      </h1>
+    </div>
+  );
+}
+
+export default App;
